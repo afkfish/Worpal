ba7001f289aa4933ab970630047c6cd1e5ac4293
afkfish <kisbeni2003@gmail.com>
2023-02-19 21:10:33 +0100

    fix: youtube_dl is a ...

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ ENV python3="/usr/local/bin/python3"
 WORKDIR /app
 RUN apt update
 RUN apt install ffmpeg -y
+RUN pip3 install git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl
 RUN pip3 install pipenv
 COPY Pipfile Pipfile.lock ./
 RUN pipenv install
--- structures/track.py
+++ structures/track.py
@@ -1,5 +1,6 @@
 import dataclasses
 import datetime as dt
+import math
 
 import discord
 from discord import VoiceChannel, Embed
@@ -30,15 +31,42 @@ class Track:
         self.channel: VoiceChannel
 
     def get_embed(self) -> Embed:
-        embed = Embed(color=0x152875, title="Track added to queue")
+        desc = f"{self.progress_bar()} `[{self.format_time()}/{self.format_time()}]`"
+
+        embed = Embed(color=0x152875, title=self.title, description=desc)
         embed.set_thumbnail(url=self.thumbnail)
-        embed.add_field(name=self.title, value=str(dt.timedelta(seconds=self.duration)), inline=False)
         if len(self.artists) > 0:
-            embed.add_field(name="Artists", value=", ".join(self.artists), inline=False)
-        # embed.set_image(self.thumbnail)
+            embed.add_field(name="Artists", value=", ".join(self.artists))
         embed.set_footer(text=f"Requested by: {self.user.name}")
 
         return embed
 
     def is_valid(self) -> bool:
         return bool(self.title and self.source)
+
+    def get_position(self) -> dt.datetime:
+        return dt.datetime.utcnow() - self.start
+
+    def get_progress(self) -> float:
+        return self.get_position().second/self.get_duration().seconds
+
+    def get_duration(self) -> dt.timedelta:
+        return dt.timedelta(seconds=self.duration)
+
+    def progress_bar(self) -> str:
+        bar = ""
+        for i in range(11):
+            if i == self.get_progress()*12:
+                bar += "⭕"
+            else:
+                bar += "▬"
+
+        return bar
+
+    def format_time(self) -> str:
+        seconds = math.floor(self.get_duration().seconds/1000.0)
+        hours = seconds/(60*60)
+        seconds %= 60*60
+        minutes = seconds/60
+        seconds %= 60
+        return str((str(hours) + ":" if hours > 0 else "") + ("0" + str(minutes) if minutes < 10 else minutes) + ":" + ("0"+ str(seconds) if seconds < 10 else seconds))
